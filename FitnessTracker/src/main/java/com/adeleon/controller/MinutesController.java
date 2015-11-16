d05ff69918a6723b4f976710438bc4ae06f4f50e
adeleong <aderso.deleon@gmail.com>
2015-11-16 14:46:13 -0400

    Add json and xml viewResolver

--- FitnessTracker/src/main/java/com/adeleon/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/adeleon/controller/MinutesController.java
@@ -1,9 +1,16 @@
 package com.adeleon.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedCaseInsensitiveMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.adeleon.model.Activity;
 import com.adeleon.model.Exercise;
 
 @Controller
@@ -13,6 +20,27 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute ("exercise")  Exercise exercite ){
 		System.out.println("exercite: "+ exercite.getMinutes() );
 		return  "addMinutes";//"redirect:addMoreMinutes.html";
+	}
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities(){
+		
+		List<Activity> activities = new ArrayList<Activity>();
+		
+		Activity run = new Activity();
+		run.setDesc("Run");
+		activities.add(run);
+		
+		Activity bike = new Activity();
+		bike.setDesc("Bike");
+		activities.add(bike);
+		
+		Activity swin = new Activity();
+		swin.setDesc("Swin");
+		activities.add(swin);
+		
+		return activities;
+		
 	}
 	
 	/*@RequestMapping(value = "/addMoreMinutes")
